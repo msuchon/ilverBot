20224d97e025edf52088dc04d816bdd9a2c04b8c
Michał Suchoń <michal.suchon@cyfroweszkoly.pl>
2024-03-01 01:21:45 +0100

    x

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='?', intents=intents)
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.command()
 async def ping(ctx):
