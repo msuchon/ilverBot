8d58dc590aed31ff2ad7b0296acd8c180bf0a704
Michał Suchoń <michal.suchon@cyfroweszkoly.pl>
2024-03-01 01:13:51 +0100

    x

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-import discord
-from discord.ext import commands
+from typing import  Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
+intents: Intents = Intents.default()
 intents.message_content = True
 
-client = commands.Bot(command_prefix='?', intents=intents)
-client.run("MTE5NTUwMDUxNDMzOTcxNzI2Mg.G5f1D1.O_8bfZM83kyszpK3E39VqDPnAoDtxqtBXXaT3c")
-client.remove_command('help')
+client: Client = Client(intents=intents)
 
 
-@client.command()
-async def text(ctx, arg):
-    await ctx.send(arg)
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+
+    def main() -> None:
+        client.run(token=TOKEN)
+
+    if __name__ == '__main__':
+        main()
